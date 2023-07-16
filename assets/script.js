03f7dcb7cfe4d3b1f6e7fa247f1434a2dcfca50f
Misachka <136197530+Misachka@users.noreply.github.com>
2023-07-16 13:32:33 -0400

    updated js file, described variables for random password

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,24 @@
 // Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var passwordcriteria = {
+    passwordlenght: 0,
+    passwordlowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    passworduppdercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    passwordnumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    passwordcharacters: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"],
+}
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
